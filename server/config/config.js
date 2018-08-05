146ce3529810d7a9e689d7403cdbc62f0a6f7290
MarcoGuzCort <marco.guzcort@gmail.com>
2018-08-04 21:14:14 -0400

    Varialble de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe'
 
 } else {
-  urlDB = 'mongodb://cafe-user:asd123123@ds113522.mlab.com:13522/cafe'
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
